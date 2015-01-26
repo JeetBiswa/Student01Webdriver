a6a1d6f70edaa6fc9f9fc220370e5b7bd46fb870
BiswajitNanda <telljeet@gmail.com>
2015-01-26 00:05:29 +0000

    after changing the name of the file as Testgmail.java

---
+++ src/test/java/webdrivermaven/TestGmail.java
@@ -0,0 +1,37 @@
+package webdrivermaven;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class TestGmail {
+
+	WebDriver driver;
+	
+	@Before
+	public void setup()
+	{
+		driver = new FirefoxDriver();
+		driver.get("http://www.gmail.com");
+				
+		
+	}
+	
+	@Test
+	public void TestGMail()
+	{
+		driver.findElement(By.id("Email")).sendKeys("Test@test.com");
+		driver.findElement(By.id("Passwd")).sendKeys("Password");
+		
+		
+	}
+	
+	@After
+	public void TearDown()
+	{
+		driver.quit();		
+	}
+}
